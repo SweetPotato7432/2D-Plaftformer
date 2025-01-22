a4512efd40f2c9b330aa5e4124ef3487b402077a
SweetPotato7432 <108255862+SweetPotato7432@users.noreply.github.com>
2025-01-22 16:57:49 +0900

    1.22
    타일 추가
    (몸상태가 영 좋지 않아서 작업 진행이 전혀 안됬음.)
    (내일은 적어도 문 배치하고 통로 뚫는 부분 구현할 필요가 있음)
    
    추후계획
    
    방 스크립트에 따라 문이 있는 곳의 이동 통로를 오픈 한다.
     - 문 스크립트를 작성하여 문이 열릴 곳의 위치의 Index의 타일을 제거(막힌 벽을 뚫는다.)
     - 위 방식이 좀 별로인거 같으면 차라리 문 있는 위치의 타일을 따로 그려서 필요할때 On/Off 하는게 좋을듯 <- 이방식이 직접 지우는게 아니라 비활성화라서 좀더 효율 좋을거 같은데.
    
    Room 스크립트
     - Room 스크립트 : 문 방향 정보, 현재 방의 좌표
      - StartRoom : 플레이어 생성(시작) 위치
      - NormalRoom : 플레이어가 들어온 방향의 생성 위치, 몬스터의 위치 및 정보, 보상 확률 계산
      - ShopRoom : 플레이어가 들어온 방향의 생성 위치, 아이템 판매 정보
      - BossRoom : 플레이어가 들어온 방향의 생성 위치, 보스 몬스터의 정보, 보상 아이템 생성 위치 및 정보,
    
     - 각 방의 이동은 GameManager가 처리
      - Room 스크립트가 문에 관한 정보를 저장하고 배치하면, 문에 캐릭터가 접촉했을 시에 GameManager의 이동 메서드 호출
    
     - 방 프리팹 다양하게 준비
     - 방 스크립트 작성
     모든 과정이 끝나거나 방 프리팹 준비 되면 플레이어 작업 시작
    
    - Door 스크립트 : 문이 생성되는걸 확인하고 배치하는 스크립트

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,71 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D body;
+    //Animator anim;
+    SpriteRenderer spriteRenderer;
+
+    [Header("Move")]
+    [SerializeField]
+    private float speed;
+    private float inputValue;
+
+    [Header("Jump")]
+    [SerializeField]
+    private float jumpPower;
+    [SerializeField]
+    bool isGround;
+
+    
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        body.linearVelocityX = inputValue * speed;
+    }
+
+    private void LateUpdate()
+    {
+        if(inputValue != 0)
+        {
+            spriteRenderer.flipX = inputValue < 0;
+        }
+    }
+
+    private void OnMove(InputValue value)
+    {
+        inputValue = value.Get<Vector2>().x;
+    }
+
+    private void OnJump(InputValue value)
+    {
+        if (isGround)
+        {
+            body.AddForceY(jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            isGround = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            isGround = false;
+        }
+    }
+
+}
