f4c1774b58d76ed07548ed2891399a3b4620bc01
SP_GoGuMa <wl7432@gmail.com>
2025-02-24 18:59:32 +0900

    텔레포트 버그 수정
    임시로 플레이어 태그로 확인,
    추후 플레이어와 적의 이동 스크립트 분리 필요

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        playerController = FindFirstObjectByType<PlayerController>();
+        playerController = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
