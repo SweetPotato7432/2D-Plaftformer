b1be1b7c20cb0e4c3990d4e540ba5bc80b5eba9f
SweetPotato7432 <108255862+SweetPotato7432@users.noreply.github.com>
2025-04-23 17:00:34 +0900

    .
    .

--- Assets/Scripts/Manager/RoomManager.cs
+++ Assets/Scripts/Manager/RoomManager.cs
@@ -2,8 +2,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.Tilemaps;
-using static Unity.VisualScripting.Member;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class RoomManager : MonoBehaviour
 {
