fd9e0887501bca9e95cd0751c3e44ad212efc586
SweetPotato7432 <108255862+SweetPotato7432@users.noreply.github.com>
2025-05-02 17:00:20 +0900

    아이템 팝업 창 구현
    아이템에 접근했을때 팝업창이 뜨게 구현
    
    추후계획
     - id 따라서 sprite교체
     - effectType에 따라서 효과 적용 구분
     - poolManaging 방식 구현
     - 드랍 아이템 UI 구현(이름, 효과 설명)

--- Assets/Scripts/Item/DropItem.cs
+++ Assets/Scripts/Item/DropItem.cs
@@ -1,8 +1,14 @@
+using TMPro;
 using UnityEngine;
 
 public class DropItem : Item
 {
     public DropItemInfo stat;
+
+    public GameObject popup;
+
+    public TMP_Text itemNameText;
+    public TMP_Text itemEffectText;
 
     private void Awake()
     {
@@ -10,8 +16,9 @@ public class DropItem : Item
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    override public void Start()
     {
+        base.Start();
         //stat = GameManager.Instance.DropItemInfoInitialize(id);
         InitalizeDropItem();
     }
@@ -27,13 +34,23 @@ public class DropItem : Item
         stat = GameManager.Instance.DropItemInfoInitialize(id);
 
         Initialize(stat.id, stat.itemName, stat.rarity, stat.effectType, stat.effectStatus, stat.effect);
+
+        popup.SetActive(false);
+
+        itemNameText.text = stat.itemName;
+        itemEffectText.text = stat.effect;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        // 플레이어와 부딪혔을때, 아이템의 상세 설명이 나오고, 활성화 키를 누르면 상호작용.
+        // 플레이어와 부딪힐때는 상세 팝업창 출현
         if (collision.gameObject.CompareTag("Player"))
         {
             Player player = collision.gameObject.GetComponent<Player>();
+
+            popup.SetActive(true);
+
 
             switch (effectType)
             {
@@ -43,6 +60,14 @@ public class DropItem : Item
                 case DropItemInfo.EffectType.Gold:
                     break;
             }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            popup.SetActive(false);
         }
     }
 
--- Assets/Scripts/Item/Item.cs
+++ Assets/Scripts/Item/Item.cs
@@ -1,8 +1,26 @@
+using System;
 using UnityEngine;
 
+[RequireComponent(typeof(Controller2D))]
 public class Item : MonoBehaviour
 {
 
+    public Controller2D controller;
+
+    float maxJumpHeight = 4;
+    float minJumpHeight = 1;
+    float timeToJumpApex =0.4f;
+
+    float accelarationTimeAirborne = .2f;
+    float accelarationTimeGrounded = .1f;
+    float moveSpeed = 12;
+
+    float gravity;
+    float defaultGravity;
+    float maxJumpVelocity;
+    float minJumpVelocity;
+    Vector3 velocity;
+    float velocityXSmoothing;
 
     public int id;
     public string itemName;
@@ -12,15 +30,42 @@ public class Item : MonoBehaviour
     public string effect;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    virtual public void Start()
     {
+        controller = GetComponent<Controller2D>();
+
+        defaultGravity = -(2 * maxJumpHeight) / Mathf.Pow(timeToJumpApex, 2);
+        gravity = defaultGravity;
+        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpApex;
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+        Debug.Log($"Item Gravity :{gravity}, JumpVelocity : {maxJumpVelocity}");
+
         
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        // 가속 및 감속 부분
+        CalculateVelocity(moveSpeed);
+        // Controller2D로 이동 및 변수 전달
+        controller.Move(velocity * Time.fixedDeltaTime, Vector2.zero, false);
+
+        //controller.Move(velocity * Time.fixedDeltaTime, false);
+
+        // 중력 초기화
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            if (controller.collisions.slidingDownMaxSlope)
+            {
+                velocity.y += controller.collisions.slopeNormal.y * -gravity * Time.fixedDeltaTime;
+            }
+            else
+            {
+                velocity.y = 0;
+            }
+        }
     }
 
     public void Initialize(int id, string itemName, int rarity, DropItemInfo.EffectType effectType, int effectStatus, string effect)
@@ -31,6 +76,14 @@ public class Item : MonoBehaviour
         this.effectType = effectType;
         this.effectStatus = effectStatus;
         this.effect = effect;
+    }
+
+    void CalculateVelocity(float moveSpeed)
+    {
+        float targetVelocityX = 0f * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref velocityXSmoothing, (controller.collisions.below) ? accelarationTimeGrounded : accelarationTimeAirborne);
+
+        velocity.y += gravity * Time.fixedDeltaTime;
     }
 
     //획득 팝업 창
